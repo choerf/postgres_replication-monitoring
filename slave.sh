59c66caf00f7cb62f2aceaa5b6e39bf6804455de
choerf <73527990+choerf@users.noreply.github.com>
2022-11-13 19:51:40 +0200

    Delete slave.sh

--- slave.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "postgresql-master:5432:*:repluser:replpass" > /var/lib/postgresql/.pgpass
-chown postgres:postgres /var/lib/postgresql/.pgpass
-chmod 0600 /var/lib/postgresql/.pgpass
